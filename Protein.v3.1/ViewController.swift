d3d4b1b4456208e7104ef97d1f57dd1e2e3d8bd1
Thao Le <thao.phuongle93@gmail.com>
2020-08-03 14:02:11 +0100

    Initial Commit

---
+++ Protein.v3.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Protein.v3.1
+//
+//  Created by Thao P Le on 03/08/2020.
+//  Copyright © 2020 Thao P Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
